f4561224ffac0cdf6db5cb86f10adf001795f756
Dan Bryntze <dan.bryntze@gmail.com>
2018-10-12 14:45:29 +0200

    Added player scene

---
+++ Player.gd
@@ -0,0 +1,41 @@
+extends Area2D
+
+export (int) var speed 
+var screensize
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+    screensize = get_viewport_rect().size
+    #hide()
+
+func _process(delta):
+    var velocity = Vector2() # The player's movement vector.
+    if Input.is_action_pressed("ui_right"):
+        velocity.x += 1
+    if Input.is_action_pressed("ui_left"):
+        velocity.x -= 1
+    if Input.is_action_pressed("ui_down"):
+        velocity.y += 1
+    if Input.is_action_pressed("ui_up"):
+        velocity.y -= 1
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * speed
+        $AnimatedSprite.play()
+    else:
+        $AnimatedSprite.stop()
+    
+    position += velocity * delta
+    position.x = clamp(position.x, 0, screensize.x)
+    position.y = clamp(position.y, 0, screensize.y)
+
+    if velocity.x != 0:
+        $AnimatedSprite.animation = "right"
+        $AnimatedSprite.flip_v = false
+        $AnimatedSprite.flip_h = velocity.x < 0
+    elif velocity.y != 0:
+        $AnimatedSprite.animation = "up"
+        $AnimatedSprite.flip_v = velocity.y > 0
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
